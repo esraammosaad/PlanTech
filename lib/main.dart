ee4280ca3276d963a44c91058d3ce89bfd6fcb1f
youmna-o <140969085+youmna-o@users.noreply.github.com>
2023-12-14 03:54:14 +0200

    2

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(debugShowCheckedModeBanner: falsegit,
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Yomna',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
