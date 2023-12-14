773d490ab381aebda24a138d6efdc7b18a109bfe
Arwa2024 <111038135+Arwa2024@users.noreply.github.com>
2023-12-14 17:07:08 +0200

    on boarding screens

---
+++ lib/core/constants/color.dart
@@ -0,0 +1,3 @@
+ import 'dart:ui';
+
+List<Color> buttonsColor =[Color(0xFF75E00A), Color(0xFF09E09F)] ;
---
+++ lib/data/models/onboardingmodel.dart
@@ -0,0 +1,13 @@
+class OnBordingModel {
+   final String? title;
+   final String? body;
+   final String? image;
+   final String? text;
+
+  OnBordingModel({
+    this.title,
+    this.body,
+    this.image,
+    this.text
+  });
+}
