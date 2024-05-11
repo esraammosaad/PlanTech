86fb5e4cb93f4119eaec44a77b20f219e5258da3
Abdelrahman Sobhy <bodeysobhy@gmail.com>
2024-05-11 03:08:42 +0300

    UI changesss

---
+++ lib/view/screens/live_stream/live_stream.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LiveStream extends StatefulWidget {
+  const LiveStream({Key? key}) : super(key: key);
+
+  @override
+  State<LiveStream> createState() => _LiveStreamState();
+}
+
+class _LiveStreamState extends State<LiveStream> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
